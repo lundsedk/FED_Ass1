6e3d11dbf407eb1fb030e34633062c100ebe87ae
lundsedk <97734662+lundsedk@users.noreply.github.com>
2023-09-28 09:55:55 +0200

    VS projekt oprettet

---
+++ FED_Ass1/FED_Ass1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FED_Ass1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
