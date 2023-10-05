623c508b0046b08e2c56d05f7bee9a1d444fa542
aolihn <98116740+aolihn@users.noreply.github.com>
2023-10-05 08:15:18 +0200

    ViewModel files added for all views

--- FED_Ass1/FED_Ass1/View/ListDebitors.xaml.cs
+++ FED_Ass1/FED_Ass1/View/ListDebitors.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FED_Ass1.ViewModel;
 
 namespace FED_Ass1.View;
 
 public partial class ListDebitors : ContentPage
 {
+	private ListViewModel vm1 = new ListViewModel();
 	public ListDebitors()
 	{
 		InitializeComponent();
+		BindingContext = vm1;
 	}
 }
---
+++ FED_Ass1/FED_Ass1/ViewModel/ListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using FED_Ass1.Data;
+using FED_Ass1.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CloudKit;
+
+namespace FED_Ass1.ViewModel
+{
+	[ObservableObject]
+	public partial class ListViewModel
+	{
+		public ListViewModel()
+		{
+			
+			// Tilføj mere her
+		}
+	}
+}
