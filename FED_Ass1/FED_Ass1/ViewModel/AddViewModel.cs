4a6e7a0123c1b6165f4693c1b1667d75789356b9
lundsedk <97734662+lundsedk@users.noreply.github.com>
2023-10-05 15:03:44 +0200

    adddebtor view and viewmodel

--- FED_Ass1/FED_Ass1/ViewModel/AddViewModel.cs
+++ FED_Ass1/FED_Ass1/ViewModel/AddViewModel.cs
@@ -4,16 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
 
 namespace FED_Ass1.ViewModel
 {
-	public partial class AddViewModel : ObservableObject
-	{
-		public AddViewModel()
-		{
+    public partial class AddViewModel : ObservableObject
+    {
+        public AddViewModel()
+        {
+            // Cstor
+        }
 
-			// Tilføj mere her
-		}
-	}
+        [ObservableProperty]
+        private string _newName;
+
+        [RelayCommand]
+        void NewDebtorEntryCompleted()
+        {
+            Console.WriteLine("Complete method");
+        }
+
+    }
 }
-
